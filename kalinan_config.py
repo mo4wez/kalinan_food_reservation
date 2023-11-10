c50097725110a313ce8e54ef4f5c33831b71f25a
moawezz <moawez.de@gmail.com>
2023-11-10 23:00:28 +0330

    add config files

---
+++ kalinan_config.py
@@ -0,0 +1,39 @@
+import os
+import json
+from pathlib import Path
+from dotenv import load_dotenv
+from exceptions import InvalidJsonConfigFileException
+
+
+class KalinanConfig:
+    def __init__(self):
+        try:
+            self.login_url, self.sms_notif, self.tele_notif = self._read_config()
+            self.api_id, self.api_hash, self.token = self._read_env_config()
+        except InvalidJsonConfigFileException:
+            exit(2)
+
+    def _read_env_config(self):
+        load_dotenv(verbose=False)
+        env_path = Path('./env') / '.env'
+        load_dotenv(dotenv_path=str(env_path))
+
+        api_id = os.getenv("API_ID")
+        api_hash = os.getenv("API_HASH")
+        token = os.getenv("TOKEN")
+
+        return api_id, api_hash, token
+
+    def _read_config(self):
+        with open('config.json') as f:
+            data = json.load(f)
+
+        if 'kalinan_login_url' not in data:
+            raise InvalidJsonConfigFileException('kalinan_login_url')
+        if 'tele_notif' not in data:
+            raise InvalidJsonConfigFileException('tele_notif')
+        if 'sms_notif' not in data:
+            raise InvalidJsonConfigFileException('sms_notif')
+
+
+        return data['kalinan_login_url'], data['sms_notif'], data['tele_notif']
