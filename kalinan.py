ab357f9a7af9dbd0aab8dc238eddb96e83871920
moawezz <moawez.de@gmail.com>
2023-11-10 23:25:53 +0330

    add method for solving captcha

--- kalinan.py
+++ kalinan.py
@@ -6,10 +6,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from kalinan_config import KalinanConfig
+from PIL import Image
 from time import sleep
 import logging
 import re
-from PIL import Image
 import easyocr
 import numpy as np
 
@@ -24,7 +24,7 @@ class Kalinan:
 
     def run(self):
         sleep(3)
-        self.login_to_kalinan()
+        self._login_to_kalinan()
 
     def _setup_driver(self):
         logging.info('Setting up driver...')
@@ -36,7 +36,7 @@ class Kalinan:
 
         return driver
     
-    def login_to_kalinan(self):
+    def _login_to_kalinan(self):
         self.driver.get(self.config.login_url)
 
         username_field = self.driver.find_element(By.XPATH, '//*[@id="txtUsernamePlain"]')
@@ -45,13 +45,24 @@ class Kalinan:
         login_button = self.driver.find_element(By.XPATH, '//*[@id="btnEncript"]')
         sleep(3)
 
+        self._download_captcha_image()
+        sleep(2)
+
         username_field.send_keys('40111913299')
         password_field.send_keys('971097209')
+        captcha_code = self._solve_captcha()
+        captcha_field.send_keys(captcha_code)
+        sleep(7)
 
-    def solve_captcha(self):
+    def _download_captcha_image(self):
+        captcha_image = self.driver.find_element(By.XPATH, '//*[@id="Img1"]')
+        captcha_image.screenshot(r'C:\Users\moawezz\Desktop\Kalinan\Captchas\cap.png')
+        logging.info('captcha image saved.')
+
+    def _solve_captcha(self):
         # Load the image using PIL
         try:
-            image = Image.open(r'./Captchas/captcha.png')
+            image = Image.open(r'C:\Users\moawezz\Desktop\Kalinan\Captchas\cap.png')
             image = np.array(image)
         except IOError as e:
             print("An error occurred while trying to load the image: ", str(e))
@@ -59,13 +70,10 @@ class Kalinan:
         # Recognize the text using easyocr
         reader = easyocr.Reader(['en'])
         result = reader.readtext(image)
-        print(result)
-
         # Extract the number from the recognized text
         number = result[0][1]
-
         # Print the extracted number
-        print(number)
+        return str(number)
 
 
 
