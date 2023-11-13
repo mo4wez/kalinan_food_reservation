9f46dbc33793a53b8795f1b934a3b0a4973bf7e0
moawezz <moawez.de@gmail.com>
2023-11-13 20:15:49 +0330

    delete old files that moved to dirs

--- exceptions.py
+++
@@ -1,4 +0,0 @@
-class InvalidJsonConfigFileException(Exception):
-    def __init__(self, msg):
-        super(InvalidJsonConfigFileException, self).__init__()
-        print(f'Invalid JSON config file. "{msg}" not found.')
